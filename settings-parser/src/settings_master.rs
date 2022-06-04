fc495ecab3073f642d17367a2efe6bfd422ec77b
SpontanCombust <cedro_p@o2.pl>
2022-06-04 21:37:02 +0200

    write settings function

--- settings-parser/src/settings_master.rs
+++ settings-parser/src/settings_master.rs
@@ -22,7 +22,10 @@ impl ToWitcherScript for SettingsMaster {
         code += &settings_class_variables(self);
         
         code += "\n";
-        code += &update_settings_function(self);
+        code += &read_settings_function(self);
+
+        code += "\n";
+        code += &write_settings_function(self);
 
         code += "}\n";
 
@@ -40,11 +43,10 @@ fn settings_class_variables(master: &SettingsMaster) -> String {
     code
 }
 
-fn update_settings_function(master: &SettingsMaster) -> String {
+fn read_settings_function(master: &SettingsMaster) -> String {
     let mut code = String::new();
 
-    //TODO rename to ReadSettings() and add WriteSettings()
-    code += "\tpublic function UpdateSettings()\n";
+    code += "\tpublic function ReadSettings()\n";
     code += "\t{\n";
 
     code += "\t\tvar config : CInGameConfigWrapper;\n";
@@ -59,10 +61,38 @@ fn update_settings_function(master: &SettingsMaster) -> String {
             get_var_value = match &var.var_type {
                 VarType::Options | VarType::SliderInt => format!("StringToInt({}, 0)", get_var_value),
                 VarType::SliderFloat => format!("StringToFloat({}, 0.0)", get_var_value),
-                _ => get_var_value
+                _ => get_var_value // bools are assigned without explicit cast from string
             };
 
             code += &format!("\t\t{}.{} = {};\n", group.name, var.name, get_var_value);
+        }
+        code += "\n";
+    }
+
+    code += "\t}\n";
+
+    code
+}
+
+fn write_settings_function(master: &SettingsMaster) -> String {
+    let mut code = String::new();
+
+    code += "\tpublic function WriteSettings()\n";
+    code += "\t{\n";
+
+    code += "\t\tvar config : CInGameConfigWrapper;\n";
+    code += "\t\tconfig = theGame.GetInGameConfigWrapper();\n";
+    code += "\n";
+
+    for group in &master.groups {
+        for var in &group.vars {
+            let var_value_str = match &var.var_type {
+                VarType::Options | VarType::SliderInt => format!("IntToString({}.{})", group.name, var.name),
+                VarType::SliderFloat => format!("FloatToString({}.{})", group.name, var.name),
+                _ => format!("{}.{}", group.name, var.name) // ShowKnown in hud.ws does no cast for bool either
+            };
+
+            code += &format!("\t\tconfig.SetVarValue('{}', '{}', {});\n", group.id, var.id, var_value_str);
         }
         code += "\n";
     }
