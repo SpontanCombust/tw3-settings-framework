4fc23bbe0d228f5805d8820b79e4b1a3129407e8
SpontanCombust <cedro_p@o2.pl>
2022-06-09 10:45:12 +0200

    refactor xml parsing

--- settings-parser/src/xml_parsing.rs
+++ settings-parser/src/xml_parsing.rs
@@ -20,75 +20,19 @@ pub fn parse_settings_xml(xml_text: String, settings_master_name: String, omit_p
         let group_nodes: Vec<Node> = root_node.children().filter(|n| n.has_tag_name("Group")).collect();
 
         if group_nodes.is_empty() {
-            println!("No Groups found inside UserConfig");
+            return Err("No Groups found inside UserConfig".to_string());
         }
         
         for group_node in &group_nodes {
-            
-            if let Some(group_id) = group_node.attribute("id") {
-                
-                if let Err(err) = validate_name(group_id) {
-                    return Err(format!("Invalid Group id {} at {}: {}", group_id, node_pos(group_node, &doc), err));
-                }
-
-                if let Some(visible_vars_node) = group_node.children().find(|n| n.has_tag_name("VisibleVars")) {
-                    let var_nodes: Vec<Node> = visible_vars_node.children().filter(|n| n.has_tag_name("Var")).collect();
-
-                    if var_nodes.is_empty() {
-                        println!("Group {} at {} has no vars and will be ignored.", group_id, node_pos(&group_node, &doc));
-                        continue;
+            match parse_group_node(group_node, &settings_master_name, &omit_prefix) {
+                Ok(group_opt) => {
+                    if let Some(group) = group_opt {
+                        master.groups.push(group);
                     }
-
-                    let mut sg = SettingsGroup::default();
-                    sg.master_name = settings_master_name.clone();
-                    sg.id = group_id.to_owned();
-                    sg.name = id_to_script_name(group_id, &omit_prefix);
-
-                    for var_node in &var_nodes {
-                        let var_id = match var_node.attribute("id") {
-                            Some(id) => id,
-                            None => {
-                                println!("Var node without id found in Group {} at {}", sg.id, node_pos(&var_node, &doc));
-                                continue;
-                            }
-                        };
-
-                        if let Err(err) = validate_name(var_id) {
-                            return Err(format!("Invalid Var id {} at {}: {}", var_id, node_pos(var_node, &doc), err));
-                        }
-                 
-                        let var_display_type = match var_node.attribute("displayType") {
-                            Some(dt) => dt,
-                            None => {
-                                println!("Var node without displayType found in Group {} at {}", sg.id, node_pos(&var_node, &doc));
-                                continue;
-                            }
-                        };
-
-                        let var_type = match VarType::from_display_type(var_display_type) {
-                            Ok(vt) => vt,
-                            Err(err) => {
-                                println!("Error parsing Var node's display_type in Group {} at {}: {}", sg.id, node_pos(&var_node, &doc), err);
-                                continue;
-                            }
-                        };
-
-
-                        sg.vars.push(SettingsVar {
-                            id: var_id.to_owned(),
-                            name: id_to_script_name(var_id, &omit_prefix),
-                            var_type: var_type
-                        });
-                    }
-
-                    master.groups.push(sg);
                 }
-                else {
-                    println!("Group {} at {} has no vars and will be ignored.", group_id, node_pos(&group_node, &doc));
+                Err(err) => {
+                    return Err(err);
                 }
-            }
-            else {
-                println!("No id attribute found for Group tag at {}", node_pos(&group_node, &doc))
             }
         }
     }
@@ -107,14 +51,97 @@ fn validate_name(name: &str) -> Result<(), String> {
         return Err("name cannot start with a number".to_string());
     }
     if name.chars().any(|c| !c.is_ascii_alphanumeric() && c != '_') {
-        return Err("name must be alphanumeric with no spaces".to_string());
+        return Err("name can only have alphanumeric characters and underscores and have no spaces".to_string());
     }
 
     return Ok(());
 }
 
-fn node_pos(node: &Node, doc: &Document) -> String {
-    let pos = doc.text_pos_at(node.range().start);
+fn parse_group_node(group_node: &Node, settings_master_name: &str, omit_prefix: &Option<String>) -> Result<Option<SettingsGroup>, String> {
+    if let Some(group_id) = group_node.attribute("id") {
+                
+        if let Err(err) = validate_name(group_id) {
+            return Err(format!("Invalid Group id {} at {}: {}", group_id, node_pos(group_node), err));
+        }
+
+        if let Some(visible_vars_node) = group_node.children().find(|n| n.has_tag_name("VisibleVars")) {
+            let var_nodes: Vec<Node> = visible_vars_node.children().filter(|n| n.has_tag_name("Var")).collect();
+
+            if var_nodes.is_empty() {
+                println!("Group {} at {} has no vars and will be ignored.", group_id, node_pos(group_node));
+                return Ok(None);
+            }
+
+            let mut sg = SettingsGroup::default();
+            sg.master_name = settings_master_name.to_owned();
+            sg.id = group_id.to_owned();
+            sg.name = id_to_script_name(group_id, omit_prefix);
+
+            for var_node in &var_nodes {
+                match parse_var_node(&var_node, group_id, omit_prefix) {
+                    Ok(var_opt) => {
+                        if let Some(var) = var_opt {
+                            sg.vars.push(var);
+                        }
+                    }
+                    Err(err) => {
+                        return Err(err);
+                    }
+                }
+            }
+
+            return Ok(Some(sg));
+        }
+        else {
+            println!("Group {} at {} has no vars and will be ignored.", group_id, node_pos(group_node));
+            return Ok(None);
+        }
+    }
+    else {
+        println!("No id attribute found for Group tag at {}", node_pos(group_node));
+        return Ok(None);
+    }
+}
+
+fn parse_var_node(var_node: &Node, group_id: &str, omit_prefix: &Option<String>) -> Result<Option<SettingsVar>, String> {
+    let var_id = match var_node.attribute("id") {
+        Some(id) => id,
+        None => {
+            println!("Var node without id found in Group {} at {}", group_id, node_pos(var_node));
+            return Ok(None);
+        }
+    };
+
+    if let Err(err) = validate_name(var_id) {
+        return Err(format!("Invalid Var id {} at {}: {}", var_id, node_pos(var_node), err));
+    }
+
+    let var_display_type = match var_node.attribute("displayType") {
+        Some(dt) => dt,
+        None => {
+            println!("Var node without displayType found in Group {} at {}", group_id, node_pos(var_node));
+            return Ok(None);
+        }
+    };
+
+    let var_type = match VarType::from_display_type(var_display_type) {
+        Ok(vt) => vt,
+        Err(err) => {
+            println!("Error parsing Var node's display_type in Group {} at {}: {}", group_id, node_pos(var_node), err);
+            return Ok(None);
+        }
+    };
+
+
+    return Ok(Some(SettingsVar {
+        id: var_id.to_owned(),
+        name: id_to_script_name(var_id, omit_prefix),
+        var_type: var_type
+    }));
+}
+
+fn node_pos(node: &Node) -> String {
+    let pos = node.document().text_pos_at(node.range().start);
     format!("line {}, column {}", pos.row, pos.col)
 }
 
